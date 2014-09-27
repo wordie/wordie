e059352ac74993c56ef214f7588940ba969e36a6
Vesa Marttila <vtmarttila@gmail.com>
2014-09-27 18:26:54 +0300

    Fix deploy

--- deployment/deployment/run.sh
+++ deployment/deployment/run.sh
@@ -10,10 +10,10 @@ setup_env()
 {
 case "$ENV" in
     local)
-        ansible-playbook -i inventory/local.ini --private-key=~/.vagrant.d/insecure_private_key -u vagrant wordie-setup.yaml
+        ansible-playbook -vvvv -i inventory/local.ini --private-key=~/.vagrant.d/insecure_private_key -u vagrant wordie-setup.yaml
         ;;
     remote)
-        ansible-playbook -i inventory/remote.ini -u cloudsigma wordie-setup.yaml
+        ansible-playbook -vvvv -i inventory/remote.ini -u cloudsigma wordie-setup.yaml
         ;;
     *)
         usage
@@ -25,10 +25,10 @@ update_env()
 {
 case "$ENV" in
     local)
-        ansible-playbook -i inventory/local.ini --private-key=~/.vagrant.d/insecure_private_key -u vagrant wordie-update.yaml
+        ansible-playbook -vvvv -i inventory/local.ini --private-key=~/.vagrant.d/insecure_private_key -u vagrant wordie-update.yaml
         ;;
     remote)
-        ansible-playbook -i inventory/remote.ini -u cloudsigma wordie-update.yaml
+        ansible-playbook -vvvv -i inventory/remote.ini -u cloudsigma wordie-update.yaml
         ;;
     *)
         usage
