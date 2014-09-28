cd72b7192efb9bbf8c1b82aaa59ca4a04aefd1ff
Vesa Marttila <vtmarttila@gmail.com>
2014-09-28 14:54:36 +0300

    Support Merriam-Webster's thesaurus in API

--- wordie-server/src/wordie_server/merriam_webster/api.clj
+++ wordie-server/src/wordie_server/merriam_webster/api.clj
@@ -14,26 +14,49 @@
 (def dictionary-url
   (str base-url "collegiate/xml/"))
 
+(def thesaurus-url
+  (str base-url "thesaurus/xml/"))
+
 (defn- zip-string
   [s]
   (zip/xml-zip (xml/parse (ByteArrayInputStream.
                             (.getBytes s "UTF-8")))))
 
-(defn parse-xml
+(defn as-xml
+  [node]
+  (clj-str/replace (with-out-str (xml/emit-element node)) "\n" ""))
+
+(defn parse-dictionary-xml
   [s]
   (let [xz (zip-string s)]
     (for [entry (xml-> xz :entry)]
       {:word (xml1-> entry :ew text)
        :spelling (xml1-> entry :hw text)
-       :definitions (for [node (xml-> entry :def :dt zip/node)]
-                      (clj-str/replace (with-out-str (xml/emit-element node))
-                                       "\n" ""))})))
+       :definitions (xml-> entry :def :dt zip/node as-xml)})))
 
-(defn build-dictionary-query-url
-  [s]
-  (str dictionary-url (URLEncoder/encode (.toLowerCase s)) "?key=" mw-keys/dictionary))
+(defn build-query-url
+  [url k s]
+  (str url (URLEncoder/encode (.toLowerCase s)) "?key=" k))
+
+(def build-dictionary-query-url
+  (partial build-query-url dictionary-url mw-keys/dictionary))
 
 (defn query-dictionary
   [s]
-  (parse-xml (slurp (build-dictionary-query-url s))))
+  (parse-dictionary-xml (slurp (build-dictionary-query-url s))))
+
+(def build-thesaurus-query-url
+  (partial thesaurus-url mw-keys/thesaurus))
+
+(defn parse-thesaurus-xml
+  [s]
+  (let [xz (zip-string s)]
+    (for [entry (xml-> xz :entry)]
+      {:word (xml1-> entry :term :ew text)
+       :spelling (xml1-> entry :term :hw text)
+       :definitions (xml-> entry :sens zip/node as-xml)})))
+
+(defn query-thesaurus
+  [s]
+  (parse-thesaurus-xml (slurp (build-thesaurus-query-url s))))
 
