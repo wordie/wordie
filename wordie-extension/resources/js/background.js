08c7edd187eb307b00ec8202004696dd8bb711e9
Pavel Prokopenko <pavel.a.prokopenko@gmail.com>
2014-09-28 11:17:29 +0300

    show and toggle on/off status

---
+++ wordie-extension/resources/js/background.js
@@ -0,0 +1,25 @@
+function updateBadge(status) {
+    if (status) {
+        chrome.browserAction.setBadgeText({'text': 'on'});
+        chrome.browserAction.setBadgeBackgroundColor({'color': '#00FF00'});
+    } else {
+        chrome.browserAction.setBadgeText({'text': 'off'});
+        chrome.browserAction.setBadgeBackgroundColor({'color': '#FF0000'});
+    }
+};
+
+chrome.storage.local.get('wordieEnabled', function(result) {
+    var wordieEnabled = result.wordieEnabled ? result.wordieEnabled : false;
+    updateBadge(wordieEnabled);
+});
+
+chrome.browserAction.onClicked.addListener(function() {
+
+    chrome.storage.local.get('wordieEnabled', function(result) {
+        var wordieEnabled = result.wordieEnabled ? result.wordieEnabled : false;
+        chrome.storage.local.set({'wordieEnabled': !wordieEnabled}, function() {
+            updateBadge(!wordieEnabled);
+        });
+    });
+    
+});
